4ea581cf763a00ab16fd2e684a98f928089dc403
miriSilverman <mirisboom@gmail.com>
2020-05-06 01:45:05 +0300

    added seekBar

---
+++ app/src/main/java/com/example/myfirstapp/FromRoot.java
@@ -0,0 +1,73 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FromRoot extends AppCompatActivity {
+    private static SeekBar seekBar;
+    private static TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_from_root);
+        seekBarOnCreate();
+    }
+
+
+
+    public void seekBarOnCreate()
+    {
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("covered: "+ seekBar.getProgress()+ " /  "+seekBar.getMax());
+
+
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progressValue;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progressValue = progress;
+                        textView.setText("covered: "+ progressValue+ " /  "+seekBar.getMax());
+                        Toast.makeText(FromRoot.this, "seek bar on progress", Toast.LENGTH_LONG).show();
+
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        Toast.makeText(FromRoot.this, "seek bar on start tracking", Toast.LENGTH_LONG).show();
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        textView.setText("covered: "+ progressValue+ " /  "+seekBar.getMax());
+                        Toast.makeText(FromRoot.this, "seek bar on stop tracking", Toast.LENGTH_LONG).show();
+
+
+                    }
+                }
+        );
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/myfirstapp/Root.java
+++ app/src/main/java/com/example/myfirstapp/Root.java
@@ -1,6 +1,7 @@
 package com.example.myfirstapp;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Root extends AppCompatActivity {
     public static Button exit;
+    public static Button next;
+
+    public void toNextActivity()
+    {
+        next =  (Button) findViewById(R.id.nextActivity);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(".FromRoot");
+                startActivity(intent);
+            }
+        });
+    }
+
 
     public void backToMainButton()
     {
@@ -76,6 +91,7 @@ public class Root extends AppCompatActivity {
 
 
         backToMainButton();
+        toNextActivity();
 
 
 
